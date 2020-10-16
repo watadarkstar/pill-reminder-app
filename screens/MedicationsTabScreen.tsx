970a55e52ab9e4c5de5f0bbc4db882c7b9f3c385
Adrian Carolli <adrian.caarolli@gmail.com>
2020-10-15 20:08:35 -0400

    Remove uneeded screen components

--- screens/MedicationsTabScreen.tsx
+++ screens/MedicationsTabScreen.tsx
@@ -1,19 +1,12 @@
 import * as React from "react";
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 
 export default function MedicationsTabScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="/screens/MedicationsTabScreen.js" />
+      <Text style={styles.title}>Medications Screen</Text>
     </View>
   );
 }
@@ -27,10 +20,5 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
   },
 });
