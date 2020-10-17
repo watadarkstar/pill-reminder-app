3107489fd562462eba385469bbfb67e51acebf99
Adrian Carolli <adrian.caarolli@gmail.com>
2020-10-16 20:34:54 -0400

    Adds react-native-calendars

--- screens/HomeTabScreen.tsx
+++ screens/HomeTabScreen.tsx
@@ -2,8 +2,9 @@ import { Fontisto } from "@expo/vector-icons";
 import * as React from "react";
 import { StyleSheet } from "react-native";
 import { FloatingAction, IActionProps } from "react-native-floating-action";
+import { Agenda } from "react-native-calendars";
 
-import { Text, View } from "../components/Themed";
+import { View } from "../components/Themed";
 
 const actions: Array<IActionProps> = [
   {
@@ -20,7 +21,30 @@ type Props = {
 const HomeTabScreen: React.FC<Props> = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home Screen</Text>
+      <Agenda
+        // The list of items that have to be displayed in agenda. If you want to render item as empty date
+        // the value of date key has to be an empty array []. If there exists no value for date key it is
+        // considered that the date in question is not yet loaded
+        items={{
+          "2020-10-15": [{ name: "item 1 - any js object" }],
+          "2012-05-23": [{ name: "item 2 - any js object", height: 80 }],
+          "2012-05-24": [],
+          "2012-05-25": [
+            { name: "item 3 - any js object" },
+            { name: "any js object" },
+          ],
+        }}
+        // Specify how empty date content with no items should be rendered
+        renderEmptyDate={() => {
+          return <View />;
+        }}
+        // Specify what should be rendered instead of ActivityIndicator
+        renderEmptyData={() => {
+          return <View />;
+        }}
+        selected={"2020-10-15"}
+        style={styles.agenda}
+      />
       <FloatingAction
         actions={actions}
         onPressItem={(name) => {
@@ -39,6 +63,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  agenda: {
+    flex: 1,
+    width: "100%",
   },
   title: {
     fontSize: 20,
